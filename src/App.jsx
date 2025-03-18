7b845586e871d0b4fef100f165971b2c5cbc7934
ogezid <rm93865@estudantes.fieb.edu.br>
2025-03-18 08:11:23 -0300

    projetopizzaria2a

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function App(){
+  const [nome, setNome] = useState('Ronaldo');
+
+  return (
+    <div>
+      <h3>Pizzaria 2A</h3>
+      <input className="Nome"
+      name= "nome"
+      placeholder="Digite seu nome"
+      onChange={(e)=>setNome(e.target.value)}
+        type="text"/>
+       <button onClick={()=>{alert(nome)}}>
+       Clique aqui  
+       </button>
+      
+    </div>
+  )
+}
+export default App
